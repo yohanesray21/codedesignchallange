df6dad1431e90e914f4de9e297ae1f95ef3b18d9
yohanessilitonga <yohanesrfsilitonga21@gmail.com>
2023-04-10 22:13:00 +0800

    update url path in homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,21 @@
 import { Inter, Source_Sans_Pro } from 'next/font/google';
-import TwitterEmbed from './beginner';
+import TwitterEmbed from './beginner/TwitterEmbed';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <TwitterEmbed />;
+  return (
+    <div className="min-h-screen flex flex-col gap-5 justify-center items-center">
+      <div>Go To : </div>
+      <ol>
+        <li className="text-blue-500 hover:text-blue-900">
+          <Link href="/beginner/TwitterEmbed">1. TwitterEmbed</Link>
+        </li>
+        <li className="text-blue-500 hover:text-blue-900">
+          <Link href="/easy/Holadoc">2. Holadoc</Link>
+        </li>
+      </ol>
+    </div>
+  );
 }
